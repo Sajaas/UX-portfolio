0361728019bfb0b9fc8b42c18399b9ec58f8253b
Sajaas <29342045+Sajaas@users.noreply.github.com>
2020-10-27 20:59:08 +0100

    changed top 5 list, flow-text

--- js/init.js
+++ js/init.js
@@ -31,7 +31,21 @@ $(document).ready(function(){
   $('.collapsible').collapsible();
 });
 
+function readMore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
 
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "show my toplist";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "hide my toplist";
+    moreText.style.display = "inline";
+  }
+}
 
 function copyToClipboard(element) {
   var $temp = $("<input>");
